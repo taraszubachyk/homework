0e40fad1e4d9e488feb26f82ec121c37f74a3687
shellbass <zubachyktm@gmail.com>
2019-09-12 17:06:41 +0300

    Task1 DONE

---
+++ Task1.py
@@ -0,0 +1,21 @@
+#Task1
+#Define variables a and b. Read values a and b from console and calculate:
+#a + b
+#a - b
+#a * b
+#a / b.
+#Output obtained results.
+
+a= float(input("Please enter A argument: "))
+b= float(input("Please enter B argument: "))
+
+addition = a+b
+print("a+b =", addition)
+subtraction = a-b
+print("a-b =", subtraction)
+multiplication = a*b
+print("a*b =", multiplication)
+division = a/b
+print("a/b =", division)
+
+print("\nEND")
