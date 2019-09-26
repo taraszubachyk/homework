50764e5a5ac2b08e101737590044c3bc18b36f28
shellbass <zubachyktm@gmail.com>
2019-09-26 14:31:37 +0300

    Done

--- Classwork_collections.py
+++ Classwork_collections.py
@@ -1,20 +1,21 @@
 print("---------------------------TASK 1-----------------------------")
 # 1.  Створити список цілих чисел, які вводяться з терміналу та визначити серед них максимальне та мінімальне число.
-# spysok= []
-#
-# while True:
-#  numbers = input("Введіть цілі числа через кому: ")
-#  if numbers=="":
-#      break
-#  numbers = int (numbers)
-#  spysok.append(numbers)
-#
-# print(spysok)
-#
-# max_number = max(spysok)
-# min_number = min(spysok)
-# print("Max number is: ", max_number)
-# print("Min number is: ", min_number)
+
+spysok= []
+
+while True:
+ numbers = input("Введіть ціле число: ")
+ if numbers=="":
+     break
+ numbers = int (numbers)
+ spysok.append(numbers)
+
+print(spysok)
+
+max_number = max(spysok)
+min_number = min(spysok)
+print("Max number is: ", max_number)
+print("Min number is: ", min_number)
 
 print("---------------------------TASK 2-----------------------------")
 # 2.  В інтервалі від 1 до 10 визначити числа
@@ -30,8 +31,16 @@ for i  in range(1,10):
     elif i%2!=0 and i%3!=0:
         print(i, end = " ")
 
+
+print(" ")
+
 print("---------------------------TASK 3-----------------------------")
 #3.  Написати програму, яка обчислює факторіал числа, яке користувач вводить.(не використовувати рекурсивного виклику функції)
+number = int (input("Введіть ціле число: "))
+result = 1
+for i in range(1,number):
+    result = result*(i+1)
+print(result)
 
 
 
@@ -45,7 +54,15 @@ login = "First"
 
 login_vvid = input("Enter your login: ")
 
-def login_check (login)
-    if login == "First"
-        print("Congrats")
-         
+# def login_check (login):
+#     if login == "First":
+#         print("Congrats")
+#     else:
+#         print("Invalid login")
+# login_check(login_vvid)
+
+if login_vvid == "First":
+    print("Congrats")
+while login_vvid != "First":
+    print("Invalid login")
+    break
