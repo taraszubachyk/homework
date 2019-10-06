ffa305e2839ec0242787bfe3b4ee689bab4d7007
shellbass <zubachyktm@gmail.com>
2019-10-07 01:42:31 +0300

    Done

--- Classwork_pygame.py
+++ Classwork_pygame.py
@@ -1,9 +1,21 @@
-from tkinter import *
+import pygame
 
-root = Tk()
-root.geometry("800x800")
-root.title("Game with ball")
+pygame.init()
+screen = pygame.display.set_mode((800, 800))
+pygame.display.set_caption("Game with ball")
 
-root.mainloop()
+run = True
+clock = pygame.time.Clock()
+
+while run:
+    pygame.time.delay(10)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    cursor = pygame.mouse.get_pos()
+    # print(cursor)
+    screen.fill((0,0,0))
+    pygame.draw.circle(screen, (80, 243, 102), (cursor[0], 400 ), 50) #cursor[1]
+    pygame.display.update()
 
 
