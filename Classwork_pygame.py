aa5e84ef807e5121619e11b3d38570f73c53f5b7
shellbass <zubachyktm@gmail.com>
2019-10-07 01:57:36 +0300

    Done

--- Classwork_pygame.py
+++ Classwork_pygame.py
@@ -4,18 +4,23 @@ pygame.init()
 screen = pygame.display.set_mode((800, 800))
 pygame.display.set_caption("Game with ball")
 
+radius = 50
 run = True
-clock = pygame.time.Clock()
 
 while run:
-    pygame.time.delay(10)
+    pygame.time.delay(50)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
     cursor = pygame.mouse.get_pos()
-    # print(cursor)
+    x = cursor[0]
+    if cursor[0] > 750:
+        x = 750
+    elif cursor[0] < 50:
+        x = radius
+    print(cursor)
     screen.fill((0,0,0))
-    pygame.draw.circle(screen, (80, 243, 102), (cursor[0], 400 ), 50) #cursor[1]
+    pygame.draw.circle(screen, (80, 243, 102), (x, 400), radius)
     pygame.display.update()
 
 
